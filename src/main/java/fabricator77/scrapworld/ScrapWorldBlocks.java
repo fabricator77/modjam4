7dce0d18b02204560e12577103eeaaee6d2b9218
fabricator77 <fabricatorgeneral@yahoo.com>
2014-05-15 12:26:59 +0930

    biome blocks auto commit

--- src/main/java/fabricator77/scrapworld/ScrapWorldBlocks.java
+++ src/main/java/fabricator77/scrapworld/ScrapWorldBlocks.java
@@ -1,5 +1,9 @@
 package fabricator77.scrapworld;
 
+import net.minecraft.block.Block;
+
 public class ScrapWorldBlocks {
+
+	public static Block scrapCube;
 
 }
--- src/main/java/fabricator77/scrapworld/biomes/ScrapMountainBiome.java
+++ src/main/java/fabricator77/scrapworld/biomes/ScrapMountainBiome.java
@@ -1,11 +1,13 @@
 package fabricator77.scrapworld.biomes;
 
+import fabricator77.scrapworld.ScrapWorldBlocks;
 import net.minecraft.world.biome.BiomeGenBase;
 
 public class ScrapMountainBiome extends BiomeGenBase {
 
 	public ScrapMountainBiome(int par1) {
 		super(par1);
+		this.topBlock = ScrapWorldBlocks.scrapCube;
 	}
 
 }
