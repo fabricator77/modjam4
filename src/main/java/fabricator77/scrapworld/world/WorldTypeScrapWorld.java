51b185715f3dc18cba5ef102c01bef48151e9abb
fabricator77 <fabricatorgeneral@yahoo.com>
2014-05-15 13:27:24 +0930

    biome blocks and world type auto commit

--- src/main/java/fabricator77/scrapworld/world/WorldTypeScrapWorld.java
+++ src/main/java/fabricator77/scrapworld/world/WorldTypeScrapWorld.java
@@ -1,6 +1,16 @@
 package fabricator77.scrapworld.world;
 
+import fabricator77.scrapworld.world.layer.GenLayerScrapBiome;
+import net.minecraft.world.World;
 import net.minecraft.world.WorldType;
+import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.biome.WorldChunkManager;
+import net.minecraft.world.biome.WorldChunkManagerHell;
+import net.minecraft.world.gen.FlatGeneratorInfo;
+import net.minecraft.world.gen.layer.GenLayer;
+import net.minecraft.world.gen.layer.GenLayerBiome;
+import net.minecraft.world.gen.layer.GenLayerBiomeEdge;
+import net.minecraft.world.gen.layer.GenLayerZoom;
 
 public class WorldTypeScrapWorld extends WorldType {
 
@@ -13,4 +23,17 @@ public class WorldTypeScrapWorld extends WorldType {
         return 192.0F;
     }
 	
+	//TODO: custom WorldChunkManager
+	public WorldChunkManager getChunkManager(World world)
+    {
+        return new WorldChunkManagerScrap(world);
+    }
+	
+	public GenLayer getBiomeLayer(long worldSeed, GenLayer parentLayer)
+    {
+        GenLayer ret = new GenLayerScrapBiome(200L, parentLayer, this);
+        ret = GenLayerZoom.magnify(1000L, ret, 2);
+        ret = new GenLayerBiomeEdge(1000L, ret);
+        return ret;
+    }
 }
---
+++ src/main/java/fabricator77/scrapworld/world/layer/GenLayerScrapBiome.java
@@ -0,0 +1,17 @@
+package fabricator77.scrapworld.world.layer;
+
+import net.minecraft.world.WorldType;
+import net.minecraft.world.gen.layer.GenLayer;
+
+public class GenLayerScrapBiome extends GenLayer {
+
+	public GenLayerScrapBiome(long par1, GenLayer parentLayer, WorldType worldType) {
+		super(par1);
+	}
+
+	@Override
+	public int[] getInts(int var1, int var2, int var3, int var4) {
+		return null;
+	}
+
+}
