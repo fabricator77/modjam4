caed2e08fd178241c2e0f34f4524cff7915b714c
fabricator77 <fabricatorgeneral@yahoo.com>
2014-05-16 10:49:58 +0930

    back to making textures, blocks and items auto commit

---
+++ src/main/java/fabricator77/scrapworld/blocks/BlockFridge.java
@@ -0,0 +1,12 @@
+package fabricator77.scrapworld.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockFridge extends Block {
+
+	public BlockFridge() {
+		super(Material.wood);
+	}
+
+}
