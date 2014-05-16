8112161b1bb74e0ab0fe751654b604d6b6193c82
fabricator77 <fabricatorgeneral@yahoo.com>
2014-05-17 03:02:59 +0930

    sleeping auto commit

--- src/main/java/fabricator77/scrapworld/blocks/TileEntityMachine.java
+++ src/main/java/fabricator77/scrapworld/blocks/TileEntityMachine.java
@@ -2,6 +2,7 @@ package fabricator77.scrapworld.blocks;
 
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
@@ -13,23 +14,52 @@ public class TileEntityMachine extends TileEntity implements IMachine {
 	private boolean complete = false;
 	private boolean powered = false;
 	private ItemStack[] parts = new ItemStack[]{}; //read/write to NBT
+	private int numParts = 3; //TODO read/write this to NBT also
 	
 	@Override
-    public void readFromNBT(NBTTagCompound par1NBTTagCompound)
+    public void readFromNBT(NBTTagCompound tag)
     {
-        super.readFromNBT(par1NBTTagCompound);
+        super.readFromNBT(tag);
 
-        if (par1NBTTagCompound.hasKey("ready"))
+        if (tag.hasKey("ready"))
         {
-            ready = par1NBTTagCompound.getBoolean("ready");
+            ready = tag.getBoolean("ready");
+        }
+        
+        NBTTagList nbttaglist = tag.getTagList("Parts", 10);
+        this.parts = new ItemStack[numParts];
+        for (int i = 0; i < nbttaglist.tagCount(); ++i)
+        {
+            NBTTagCompound nbttagcompound1 = nbttaglist.getCompoundTagAt(i);
+            int j = nbttagcompound1.getByte("Slot") & 255;
+
+            if (j >= 0 && j < this.parts.length)
+            {
+                this.parts[j] = ItemStack.loadItemStackFromNBT(nbttagcompound1);
+            }
         }
     }
 	
 	@Override
-    public void writeToNBT(NBTTagCompound par1NBTTagCompound)
+    public void writeToNBT(NBTTagCompound tag)
     {
-        super.writeToNBT(par1NBTTagCompound);
-        par1NBTTagCompound.setBoolean("ready", this.ready);
+        super.writeToNBT(tag);
+        tag.setBoolean("ready", this.ready);
+        
+        NBTTagList nbttaglist = new NBTTagList();
+
+        for (int i = 0; i < this.parts.length; ++i)
+        {
+            if (this.parts[i] != null)
+            {
+                NBTTagCompound nbttagcompound1 = new NBTTagCompound();
+                nbttagcompound1.setByte("Slot", (byte)i);
+                this.parts[i].writeToNBT(nbttagcompound1);
+                nbttaglist.appendTag(nbttagcompound1);
+            }
+        }
+
+        tag.setTag("Parts", nbttaglist);
     }
 
 
