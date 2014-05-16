d9b4bf36fa95bae4249333e266cfe29070a53161
fabricator77 <fabricatorgeneral@yahoo.com>
2014-05-16 11:05:10 +0930

    back to making textures, blocks and items auto commit

--- src/main/java/fabricator77/scrapworld/ScrapWorldBlocks.java
+++ src/main/java/fabricator77/scrapworld/ScrapWorldBlocks.java
@@ -2,6 +2,7 @@ package fabricator77.scrapworld;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import fabricator77.scrapworld.blocks.BlockCompressedScrapCube;
+import fabricator77.scrapworld.blocks.BlockFridge;
 import fabricator77.scrapworld.blocks.BlockScrapCube;
 import fabricator77.scrapworld.blocks.BlockScrapFurnace;
 import fabricator77.scrapworld.blocks.BlockScrapSurfaceDebris;
@@ -27,6 +28,7 @@ public class ScrapWorldBlocks {
 	public static Block wireTangle;
 	public static Block scrapFurnace;
 	public static Block litScrapFurnace;
+	public static Block fridge;
 	
 	public static Item scrapItems1;
 	public static Item dusts;
@@ -48,6 +50,11 @@ public class ScrapWorldBlocks {
 		GameRegistry.registerBlock(scrapFurnace, "scrapFurnace");
 		litScrapFurnace = new BlockScrapFurnace(false).setHardness(3.5F).setBlockName("litScrapFurnace");
 		GameRegistry.registerBlock(litScrapFurnace, "litScrapFurnace");
+		
+		
+		//TODO: customize renderer to suit
+		fridge = new BlockFridge(0).setHardness(3.5F).setBlockName("fridge");
+		GameRegistry.registerBlock(fridge, "fridge");
 		
 		
 		scrapItems1 = new ScrapItems1();
--- src/main/java/fabricator77/scrapworld/blocks/BlockFridge.java
+++ src/main/java/fabricator77/scrapworld/blocks/BlockFridge.java
@@ -1,12 +1,22 @@
 package fabricator77.scrapworld.blocks;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import fabricator77.scrapworld.ScrapWorld;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockChest;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 
-public class BlockFridge extends Block {
+public class BlockFridge extends BlockChest {//extends Block {
 
-	public BlockFridge() {
-		super(Material.wood);
+	public BlockFridge(int state) {
+		super(state);
 	}
 
+	@SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister iconRegister)
+    {
+        this.blockIcon = iconRegister.registerIcon(ScrapWorld.modid+":fridge");
+    }
 }
