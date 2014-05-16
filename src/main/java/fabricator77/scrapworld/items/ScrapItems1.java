a35614abc35a528289b6ed43b46b6b241ebff3d5
fabricator77 <fabricatorgeneral@yahoo.com>
2014-05-16 19:54:23 +0930

    back to making textures, blocks and items auto commit

--- src/main/java/fabricator77/scrapworld/items/ScrapItems1.java
+++ src/main/java/fabricator77/scrapworld/items/ScrapItems1.java
@@ -141,7 +141,7 @@ public class ScrapItems1 extends Item {
     		// choose a random type to make this into
     		// scrapType = itemNames[1 + world.rand.nextInt(itemNames.length-1)];
     		boolean added = player.inventory.addItemStackToInventory(new ItemStack(this, 1, 1 + world.rand.nextInt(itemNames.length-1)));
-    		--itemStack.stackSize;
+    		//--itemStack.stackSize;
 			return itemStack;
     	}
     	
