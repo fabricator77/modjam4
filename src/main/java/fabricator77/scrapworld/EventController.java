d18d874daf24129843009cd85a748c30979135ed
fabricator77 <fabricatorgeneral@yahoo.com>
2014-05-17 01:57:46 +0930

    back to making textures, blocks and items auto commit

--- src/main/java/fabricator77/scrapworld/EventController.java
+++ src/main/java/fabricator77/scrapworld/EventController.java
@@ -5,6 +5,7 @@ import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
+import net.minecraft.nbt.NBTTagString;
 
 public class EventController {
 	//TODO: need to look up Player Join / Player spawn / World creation events
@@ -12,6 +13,13 @@ public class EventController {
 		ItemStack book = new ItemStack(Items.written_book);
 		
 		NBTTagCompound tag = new NBTTagCompound();
-		NBTTagList bookPages = new NBTTagList();		
+		NBTTagList bookPages = new NBTTagList();
+		
+		bookPages.appendTag(new NBTTagString("In the distant future, you find yourself stranded in a world which seems alien today."));
+		bookPages.appendTag(new NBTTagString("The ruined remains and literal mountains of scrap are all you have around you."));
+		
+		book.setTagInfo("pages", bookPages);
+		book.setTagInfo("author", new NBTTagString("fabricator77"));
+		book.setTagInfo("title", new NBTTagString("How did it come to this ?"));
 	}
 }
