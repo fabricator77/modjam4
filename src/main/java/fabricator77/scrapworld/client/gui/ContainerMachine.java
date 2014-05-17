d4398699222b8900dc7838c259b935555e5d9fcb
fabricator77 <fabricatorgeneral@yahoo.com>
2014-05-17 14:08:05 +0930

    machines and textures auto commit

--- src/main/java/fabricator77/scrapworld/client/gui/ContainerMachine.java
+++ src/main/java/fabricator77/scrapworld/client/gui/ContainerMachine.java
@@ -4,15 +4,85 @@ import fabricator77.scrapworld.blocks.TileEntityMachine;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 
 public class ContainerMachine extends Container {
+	
+	private TileEntityMachine tileEntity;
 
 	public ContainerMachine(InventoryPlayer inventory, TileEntityMachine tileentityMachine) {
+		this.tileEntity = tileentityMachine;
+        int i;
+        int j;
+
+        for (i = 0; i < 3; ++i)
+        {
+            for (j = 0; j < 3; ++j)
+            {
+                this.addSlotToContainer(new Slot(tileentityMachine, j + i * 3, 62 + j * 18, 17 + i * 18));
+            }
+        }
+
+        for (i = 0; i < 3; ++i)
+        {
+            for (j = 0; j < 9; ++j)
+            {
+                this.addSlotToContainer(new Slot(inventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
+            }
+        }
+
+        for (i = 0; i < 9; ++i)
+        {
+            this.addSlotToContainer(new Slot(inventory, i, 8 + i * 18, 142));
+        }
 	}
 
 	@Override
-	public boolean canInteractWith(EntityPlayer var1) {
-		return false;
+	public boolean canInteractWith(EntityPlayer player) {
+		return this.tileEntity.isUseableByPlayer(player);
 	}
 
+	@Override
+	public ItemStack transferStackInSlot(EntityPlayer player, int slotInt)
+    {
+        ItemStack itemstack = null;
+        Slot slot = (Slot)this.inventorySlots.get(slotInt);
+
+        if (slot != null && slot.getHasStack())
+        {
+            ItemStack itemstack1 = slot.getStack();
+            itemstack = itemstack1.copy();
+
+            if (slotInt < 9)
+            {
+                if (!this.mergeItemStack(itemstack1, 9, 45, true))
+                {
+                    return null;
+                }
+            }
+            else if (!this.mergeItemStack(itemstack1, 0, 9, false))
+            {
+                return null;
+            }
+
+            if (itemstack1.stackSize == 0)
+            {
+                slot.putStack((ItemStack)null);
+            }
+            else
+            {
+                slot.onSlotChanged();
+            }
+
+            if (itemstack1.stackSize == itemstack.stackSize)
+            {
+                return null;
+            }
+
+            slot.onPickupFromSlot(player, itemstack1);
+        }
+
+        return itemstack;
+    }
 }
