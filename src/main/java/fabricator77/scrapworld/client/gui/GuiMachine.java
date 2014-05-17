55a4c8446e126da575f14a2362adaf4a0765418a
fabricator77 <fabricatorgeneral@yahoo.com>
2014-05-17 13:37:54 +0930

    machines and textures auto commit

--- src/main/java/fabricator77/scrapworld/client/gui/GuiMachine.java
+++ src/main/java/fabricator77/scrapworld/client/gui/GuiMachine.java
@@ -1,5 +1,20 @@
 package fabricator77.scrapworld.client.gui;
 
-public class GuiMachine {
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.inventory.Container;
+
+public class GuiMachine extends GuiContainer{
+
+	public Container container;
+
+	public GuiMachine(Container container) {
+		super(container);
+		this.container = container;
+	}
+
+	@Override
+	protected void drawGuiContainerBackgroundLayer(float f, int x, int y) {
+		
+	}
 
 }
