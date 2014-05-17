9b61095beb6143f184610adc9b91fcc71bd23691
fabricator77 <fabricatorgeneral@yahoo.com>
2014-05-18 01:03:05 +0930

    extra machines auto commit

---
+++ src/main/java/fabricator77/scrapworld/blocks/TileEntityStaticGenerator.java
@@ -0,0 +1,26 @@
+package fabricator77.scrapworld.blocks;
+
+import net.minecraft.world.World;
+
+public class TileEntityStaticGenerator extends TileEntityMachine{
+
+	@Override
+	public String getInventoryName() {
+		return "container.generator."+((IMachineBlock)this.blockType).getMachineName();
+	}
+	
+	@Override
+	public String getMachineName() {
+		return ((IMachineBlock)this.blockType).getMachineName();
+	}
+	
+	@Override
+	public void getPower () {
+		//TODO: is on mains power, or contains powerCell
+		int power = ((BlockGenerator)this.blockType).getPowerLevel(this.worldObj, this.xCoord, this.yCoord, this.zCoord);
+	}
+	
+	public int getPowerLevel(World world, int x, int y, int z) {
+		return 0;
+	}
+}
